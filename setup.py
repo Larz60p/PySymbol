20ff8ab54e985b53a31ad84c17945017ff1820c8
Larz60+ <Larz60p@users.noreply.github.com>
2019-11-14 11:37:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'PySymbol',         # How you named your package folder (MyLib)
   packages = ['PySymbol'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '1.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'US Stock Market Symbol/Company lookup',   # Give a short description about your library
   author = 'Larz60+',                   # Type in your name
@@ -17,7 +17,7 @@ setup(
           'lxml'
       ],
   classifiers=[
-    'Development Status :: 4',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
