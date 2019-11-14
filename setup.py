c62c7e6b8ba862324e1742cbf32be7a30040fb2a
Larz60+ <Larz60p@users.noreply.github.com>
2019-11-14 11:30:43 -0500

    Update setup.py
    fixed release

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
           'lxml'
       ],
   classifiers=[
-    'Development Status :: 1 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 4',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
